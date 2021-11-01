0f6c2592bfbbee13a377f34600c4e8c019254289
edcilo <edcilop@gmail.com>
2021-11-01 17:49:06 -0600

    add cors support

--- ms/__init__.py
+++ ms/__init__.py
@@ -1,9 +1,14 @@
+from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 
 
+load_dotenv()
 app = Flask(__name__)
+CORS(app)
 
 
+import ms.config
 import ms.config
 import ms.urls
 import ms.models

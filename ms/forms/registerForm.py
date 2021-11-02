9d837737edde8e79c3616b89fbb48f2f85061823
edcilo <edcilop@gmail.com>
2021-11-02 02:41:02 -0600

    fix username validations

--- ms/forms/registerForm.py
+++ ms/forms/registerForm.py
@@ -1,21 +1,26 @@
 from ms.models.user import User
 from ms.helpers.validators import Unique
 from flask_wtf import FlaskForm
+from .form import Form
 from wtforms import StringField
 from wtforms.validators import (
     DataRequired,
     Email,
     Length,
+    Regexp
 )
 
 
-class RegisterForm(FlaskForm):
+class RegisterForm(Form):
     class Meta:
         csrf = False
 
     username = StringField('username', validators=[
         DataRequired(),
         Length(min=3, max=120),
+        Regexp(
+            '^\\w+$',
+            message="Username must contain only letters numbers or underscore"),
         Unique(User)])
     password = StringField('password', validators=[
         DataRequired(),

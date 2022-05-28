74ecd83a70feb9ed85389dada9e7b47531f62673
edcilo <edcilop@gmail.com>
2022-05-27 23:47:24 -0500

    fix redis configuration

--- ms/config/cacheConfig.py
+++ ms/config/cacheConfig.py
@@ -4,7 +4,7 @@ import os
 cache_config = {
     "REDIS": {
         "HOST": os.getenv("REDIS_HOST", "127.0.0.1"),
-        "USERNAME": os.getenv("REDIS_USERNAME"),
+        "USERNAME": os.getenv("REDIS_USERNAME", "default"),
         "PASSWORD": os.getenv("REDIS_PASSWORD"),
         "PORT": os.getenv("REDIS_PORT", "6379"),
         "DATABASE": os.getenv("REDIS_DB", "0"),
--- ms/tasks/worker.py
+++ ms/tasks/worker.py
@@ -6,9 +6,13 @@ redisConfig = app.config.get("REDIS", dict())
 redisHost = redisConfig.get("HOST")
 redisPost = redisConfig.get("PORT")
 redisDB = redisConfig.get("DATABASE")
+redisUser = redisConfig.get("USERNAME")
+redisPass = redisConfig.get("PASSWORD")
+
+auth = f'{redisUser}:{redisPass}' if len(redisPass) > 0 else redisUser
 
 
 celery = Celery('worker')
-celery.conf.broker_url = f'redis://{redisHost}:{redisPost}/1'
-celery.conf.result_backend = f'redis://{redisHost}:{redisPost}/2'
+celery.conf.broker_url = f'redis://{auth}@{redisHost}:{redisPost}/1'
+celery.conf.result_backend = f'redis://{auth}@{redisHost}:{redisPost}/2'
 celery.conf.imports = ['ms.tasks', 'ms.tasks.updateCache', ]

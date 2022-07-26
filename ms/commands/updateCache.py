fe15f8ed8612d955e637c023455df592ad0c3175
edcilo <edcilop@gmail.com>
2022-07-26 00:02:48 -0500

    add applications to cache

--- ms/commands/updateCache.py
+++ ms/commands/updateCache.py
@@ -2,6 +2,7 @@ import click
 import os
 from flask.cli import with_appcontext
 from ms.repositories import UserRepository
+from ms.repositories import AppRepository
 
 
 @click.command(name='updateCache',
@@ -9,4 +10,7 @@ from ms.repositories import UserRepository
 @with_appcontext
 def updatecache():
     userRepo = UserRepository()
+    appRepo = AppRepository()
+
     userRepo.updateCache()
+    appRepo.updateCache()
--- ms/repositories/appRepository.py
+++ ms/repositories/appRepository.py
@@ -1,4 +1,5 @@
 from sqlalchemy import or_
+from ms import app as ms_app
 from ms.models import App
 from .repository import Repository
 from .permissionRepository import PermissionRepository
@@ -6,8 +7,17 @@ from .roleRepository import RoleRepository
 
 
 class AppRepository(Repository):
+    def __init__(self) -> None:
+        super().__init__()
+        self.cache_key_prefix = "ms-users-"
+
     def get_model(self):
         return App
+
+    def add(self, data):
+        app = super().add(data)
+        self.setCache(app)
+        return app
 
     def all(self,
             search=None,
@@ -33,6 +43,7 @@ class AppRepository(Repository):
             permission = permissionRepo.find(permission_id)
             app.permissions.append(permission)
         self.db_save(app)
+        self.setCache(app)
 
     def sync_roles(self, id, roles):
         roleRepo = RoleRepository()
@@ -42,3 +53,28 @@ class AppRepository(Repository):
             role = roleRepo.find(role_id)
             app.roles.append(role)
         self.db_save(app)
+        self.setCache(app)
+
+    def delete(self, id):
+        app = super().delete(id)
+        self.deleteCache(app)
+
+    def updateCache(self) -> bool:
+        apps = self.all()
+        ms_app.cache.truncate(self.cache_key_prefix)
+        for app in apps:
+            self.setCache(app)
+        return True
+
+    def setCache(self, app):
+        permissions = [p.name for p in app.all_permissions]
+        data = {
+            "id": app.id,
+            "name": app.name,
+            "permissions": permissions,
+            "roles": app.roles_list
+        }
+        ms_app.cache.set(f"{self.cache_key_prefix}{app.id}", data)
+
+    def deleteCache(self, app):
+        ms_app.cache.delete(f"{self.cache_key_prefix}{app.id}")

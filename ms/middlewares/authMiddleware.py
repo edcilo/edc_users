f565277d81cd21e28652a3fac7a6f76b72d619df
edcilo <edcilop@gmail.com>
2022-08-04 09:41:53 -0500

    fix authmiddleware for apps

--- ms/middlewares/authMiddleware.py
+++ ms/middlewares/authMiddleware.py
@@ -1,6 +1,6 @@
 from flask import abort
 from ms.helpers.jwt import JwtHelper
-from ms.repositories import UserRepository
+from ms.repositories import UserRepository, AppRepository
 from .middleware import Middleware
 
 
@@ -8,22 +8,25 @@ class AuthMiddleware(Middleware):
     def handler(self, request):
         jwtHelper = JwtHelper()
         userRepo = UserRepository()
+        appRepo = AppRepository()
         auth = request.headers.get('Authorization')
 
         if not auth:
             abort(401)
 
         valid = jwtHelper.check(auth)
-
         if not valid:
             abort(401)
 
         payload = jwtHelper.decode(auth)
-        user = userRepo.find(payload['id'], fail=False)
-        if not user:
-            abort(401)
+        entity = userRepo.find(payload['id'], fail=False)
 
-        payload['user'] = user
+        if not entity:
+            entity = appRepo.find(payload['id'], fail=False)
+            if not entity:
+                abort(401)
+
+        payload['user'] = entity
         setattr(request, 'auth', payload)
 
         return True
